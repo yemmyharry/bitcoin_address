1fb0ccdae0cac591de8f399ef67aed2766797db2
yemmyharry <yemmyharry@gmail.com>
2024-01-29 22:04:46 +0100

    refactored code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
@@ -10,19 +9,28 @@ import (
 )
 
 func main() {
+	createP2PKHAddress()
 
+}
+
+func createP2PKHAddress() {
+	network := &chaincfg.TestNet3Params
 	privKey, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	pubKey := privKey.PubKey()
-	addr, err := btcutil.NewAddressPubKeyHash(btcutil.Hash160(pubKey.SerializeCompressed()), &chaincfg.TestNet3Params)
+	addr, err := btcutil.NewAddressPubKeyHash(btcutil.Hash160(pubKey.SerializeCompressed()), network)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wif, err := btcutil.NewWIF(privKey, network, true)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf(" Public Address is %s\n", addr)
-	fmt.Printf(" Private Key is %s\n", hex.EncodeToString(privKey.Serialize()))
-
+	fmt.Printf(" Private Key is %s\n", wif.String())
 }
